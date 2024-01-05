d04f5f63e681611cfdc4d881dc7aa233dba561fe
Rhys Frazier <rhysrfrazier@gmail.com>
2024-01-05 16:39:04 -0500

    styling is no longer atrocious for mobile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,24 @@
 import { Link, useLocation } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function Nav() {
 
     const location = useLocation()
+    const [burgerState, setBurgerState] = useState(false)
 
     const makeActive = (routeSlug) => {
         if (routeSlug === location.pathname) {
             return 'selected'
         } else {
             return ''
+        }
+    }
+
+    const revealMenu = () => {
+        if (burgerState === false) {
+            setBurgerState(true)
+        } else {
+            setBurgerState(false)
         }
     }
 
@@ -28,13 +38,41 @@ export default function Nav() {
                 <Link to='/projects' >
                     <h2 className={makeActive('/projects')}>Projects</h2>
                 </Link>
-                {/* <Link to='/resume' >
-                <h2 className={makeActive('/resume')}>Resume</h2>
-            </Link> */}
                 <Link to='/contact' >
                     <h2 className={makeActive('/contact')}>Get in Touch</h2>
                 </Link>
             </div>
+            <button className='navBurger' onClick={revealMenu}>
+                <i className="fa-solid fa-bars fa-3x" aria-hidden='true'>
+                    <span className='fa-sr-only'>Open navigation menu</span>
+                </i>
+            </button>
+            {burgerState ? <div className='burgerMenu'>
+                <Link to='/' onClick={revealMenu}>
+                    <h2 className={makeActive('/')}>About</h2>
+                </Link>
+                <Link to='/resume' onClick={revealMenu}>
+                    <h2 className={makeActive('/resume')}>Skills and Experience</h2>
+                </Link>
+                <Link to='/projects' onClick={revealMenu}>
+                    <h2 className={makeActive('/projects')}>Projects</h2>
+                </Link>
+                <Link to='/contact' onClick={revealMenu}>
+                    <h2 className={makeActive('/contact')}>Get in Touch</h2>
+                </Link>
+                <Link to='https://github.com/rhysrfrazier' target='_blank' onClick={revealMenu}>
+                    <h2>GitHub</h2>
+                </Link>
+                <Link to='https://www.linkedin.com/in/rhysrfrazier/' target='_blank' onClick={revealMenu}>
+                    <h2>LinkedIn</h2>
+                </Link>
+                <Link to='mailto:rhysrfrazier@gmail.com' target='_blank' onClick={revealMenu}>
+                    <h2>Email Me</h2>
+                </Link>
+                <Link to='Rhys_Frazier_Resume.pdf' target='_blank' download onClick={revealMenu}>
+                    <h2>Download My Resume</h2>
+                </Link>
+            </div> : null}
         </div>
     )
 }
