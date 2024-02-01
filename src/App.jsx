120d3596159d6241ecfab1c0392173903eeda057
Rhys Frazier <rhysrfrazier@gmail.com>
2024-02-01 17:21:10 -0500

    started revising for scrollable

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
 import Nav from './components/Nav'
-import Main from'./components/Main'
+import About from './components/About'
+import Contact from './components/Contact'
+import Projects from './components/Projects'
+import Resume from './components/Resume'
 import Footer from './components/Footer'
 
 
@@ -8,11 +11,16 @@ function App() {
 
 
   return (
-   <div className='app'>
+    <div className='app'>
       <Nav />
-      <Main />
+      <About />
+      {/* need to redo the skills and resume section because it gets in the way of the about as it is */}
+      {/* need to redo the formatting of projects - full thing without modals because fuck me ig */}
+      <Projects />
+      <Contact />
       <Footer />
-   </div>
+
+    </div>
   )
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,15 @@
-import { Route, Routes } from 'react-router-dom'
-import About from './About'
-import Contact from './Contact'
-import Projects from './Projects'
-import Resume from './Resume'
+// import About from './About'
+// import Contact from './Contact'
+// import Projects from './Projects'
+// import Resume from './Resume'
 
-export default function Main(){
-    return(
-        <div className='routesContainer'>
-            <Routes>
-                <Route path='/' element={<About/>} />
-                <Route path='/contact' element={<Contact/>} />
-                <Route path='/projects' element={<Projects/>} />
-                <Route path='/resume' element={<Resume/>} />
-            </Routes>
-        </div>
-    )
-}
+// export default function Main() {
+//     return (
+//         <div className='routesContainer'>
+//             <About />
+//             {/* <Projects />
+//             <Resume />
+//             <Contact /> */}
+//         </div>
+//     )
+// }
