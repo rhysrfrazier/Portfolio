b5f9beffcd1107d019b98d4a62558c4e71f1c0de
Rhys Frazier <rhysrfrazier@gmail.com>
2024-02-05 22:00:36 -0500

    redid about section (mobile)

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,25 @@ export default function About() {
             <div className='heyContainer'>
                 <div className='heyThere'>
                     <h1>Hey There!</h1>
-                    <p>My name's Rhys! I'm here, I'm queer, and I'm a junior engineer - with an interest in accessible design and an eye for nit-picky details. My MA is in English, but coding is more fun than copywriting, so I traded in Word for VSCode. Ask me about the similarities between writing a paper about Old Norse wisdom literature and coding a website from scratch!</p>
+                    <p>My name's Rhys! I'm here, I'm queer, and I'm a software engineer. My MA is in English, but coding is more fun than copywriting, so I traded in Word for VSCode. Ask me about the similarities between writing a paper about Old Norse wisdom literature and coding a website from scratch!</p>
+                    <div className='likesnDislikes'>
+                        <ul className='likes'>
+                            <p>Likes:</p>
+                            <li>Helping folks</li>
+                            <li>Accessible technology</li>
+                            <li>Learning new things</li>
+                            <li>Robust, well-written docs</li>
+                            <li>Winning whack-a-mole with buggy code</li>
+                            <li>Writing</li>
+                            <li>Coding - of course!</li>
+                        </ul>
+                        <ul className='dislikes'>
+                            <p>Dislikes:</p>
+                            <li>When folks are rude to service workers</li>
+                            <li>Unclear expectations</li>
+                            <li>Oatmeal</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
